4423e611b0c4e73ffb283550b6d098409cc6a686
juandaabril <juandaabril@gmail.com>
2023-11-12 10:16:45 -0500

    move folders

--- oop-encapsulation/ruby/src/entity/student.rb
+++
@@ -1,3 +0,0 @@
-class Student
-  attr_accessor :id, :name, :score, :is_eligible_for_free_tuition
-end
--- oop-encapsulation/ruby/src/repository/student_repository.rb
+++
@@ -1,5 +0,0 @@
-class StudentRepository
-  def save(student)
-    raise NotImplementedError, 'Method save() must be implemented'
-  end
-end
